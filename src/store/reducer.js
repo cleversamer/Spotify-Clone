b2f9449c6b3b322ca1e17fc9e99d1abc71abf468
Samer A <thedev.samer@gmail.com>
2022-04-13 16:30:16 +0300

    re-structured the store directory

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import entities from "./entities";
-import user from "./user";
-import playing from "./playing";
-import item from "./item";
-import spotify from "./spotify";
+import entities from "./reducers/entities";
+import user from "./reducers/user";
+import playing from "./reducers/playing";
+import item from "./reducers/item";
+import spotify from "./reducers/spotify";
 
 export default combineReducers({ entities, user, spotify, item, playing });
