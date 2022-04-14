5dd666c427c06faef10a611fef86affa8aa6b0e8
Samer A <thedev.samer@gmail.com>
2022-04-14 17:29:38 +0300

    removed unnecessary reducers

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,16 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import entities from "./reducers/entities";
 import user from "./reducers/user";
 import playing from "./reducers/playing";
-import item from "./reducers/item";
-import spotify from "./reducers/spotify";
 import discoverWeekly from "./reducers/discoverWeekly";
 import openedList from "./reducers/openedList";
 
 export default combineReducers({
   entities,
   user,
-  spotify,
-  item,
   playing,
   discoverWeekly,
   openedList,
